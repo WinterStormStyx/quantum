2b4c9b1c7d731cba00acaba969cf72bfdcc3643b
Joaquin <joaquincurt@gmail.com>
2025-01-17 11:13:22 +0100

    update

--- Hadamard.py
+++ Hadamard.py
@@ -9,22 +9,22 @@ from qiskit_ibm_runtime.fake_provider import FakeAlmadenV2
 
 from matplotlib import pyplot as plt
 import numpy as np
-from shor import shor, measurement
+from shor import shor, measurement,shorEncode
 
 def applylogicalHadamard(qc):
 
     qc = QuantumCircuit(9)
  
-    qc.ccx([6,7],8)
+    qc.ccx(6,7,8)
     for i in range(8):
         qc.x(i)
     qc.z(8)
     qc.h(8)
-    qc.ccx([0,1,2,3,4,5,6,7], 8)
+    qc.cx([0,1,2,3,4,5,6,7], 8)
     for i in range(8):
         qc.x(i)
-    qc.ccx([6,7],8)
+    qc.ccx(6,7,8)
     qc.draw("mpl")
     plt.show()
 
-applylogicalHadamard(shor("0", biterrors=None, phaseerrors=None, drawCircuit=True, drawStates=False))
+applylogicalHadamard(shorEncode("0"))
