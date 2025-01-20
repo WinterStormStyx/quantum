8dfd346294d9999cdff7ab0b2f07df40062684ad
Em Ketterer <E.L.Ketterer@student.tudelft.nl>
2025-01-20 17:33:38 +0100

    changes to method call

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ from Basis import Basis
 # Shor
 
 # biterror on 8 does not always work, phaserror on 7 and 8
-for state in ["+", "-"]:
-    for error in range(6):
-        qc = shor(state, biterrors=error, phaseerrors=error)
-
-        print(simulate(qc, Basis.Z))
+for state in ["+", "-", "0", "1"]:
+    print(state)
+    for error in range(9):
+        #qc = shor(state, biterrors=error)
+        print(simulate(shor(state, biterrors=error, phaseerrors=error), Basis.Z), simulate(shor(state, biterrors=error, phaseerrors=error), Basis.X))
+    print("")
