26c6570c4e1656f2865f38bd41c65accc1421812
mariatantos <m.tantos@student.rug.nl>
2025-01-22 12:21:46 +0100

    some minor changes and testing, just making sure everything is comitted

--- main.py
+++ main.py
@@ -2,26 +2,36 @@ from qiskit import QuantumCircuit
 from basis import Basis
 from matplotlib import pyplot as plt
 from shor_cnot import shorDecode, shorEncode, applyLogicalCNot, simulate
+import numpy as np
 
-# initialize the two logical qubits
-qc1 = shorEncode("1")
-qc2 = shorEncode("1")
+for error in range(9):
+    # initialize the two logical qubits
+    qc1 = shorEncode("1")
+    qc2 = shorEncode("0")
 
-# build the circuit of two logical qubits
-qc = QuantumCircuit(18, 2)
-qc.compose(qc1, range(9), inplace = True)
-qc.compose(qc2, range(9,18), inplace = True)
-qc.barrier()
+    # build the circuit of two logical qubits
+    qc = QuantumCircuit(18, 2)
+    qc.compose(qc1, range(9), inplace = True)
+    qc.compose(qc2, range(9,18), inplace = True)
+    qc.barrier()
 
-applyLogicalCNot(qc)
-qc.barrier()
+    error2 = np.random.randint(9, 18) # errors will occur on this random bit in the second logical qubit
+    qc.x(error)
+    qc.x(error2)
+    
+    qc.z(error)
+    qc.z(error2)
 
-qc = shorDecode(qc, 0)  # Decode the first logical qubit
-qc = shorDecode(qc, 9)  # Decode the second logical qubit
-qc.barrier()
+    applyLogicalCNot(qc)
+    qc.barrier()
 
-qc.draw("mpl")
-plt.title("Logical qubit 18")
-plt.show()
+    qc = shorDecode(qc, 0)  # Decode the first logical qubit
+    qc = shorDecode(qc, 9)  # Decode the second logical qubit
+    qc.barrier()
 
-print(simulate(qc, Basis.Z))
+    # Draw circuit
+    # qc.draw("mpl")
+    # plt.title("Logical qubit 18")
+    # plt.show()
+
+    print("Error on bits ", error, ", ", error2, " ", simulate(qc, Basis.Z))
